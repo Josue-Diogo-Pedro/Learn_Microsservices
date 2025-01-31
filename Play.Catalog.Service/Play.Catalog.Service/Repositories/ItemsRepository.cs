cf068e6fcc2f8dd6dc4cb598d0103f6c09c7a612
Josué <pedrojosuediogo@gmail.com>
2025-01-31 15:27:41 +0100

    Play.Catalog.Service - add basic methods for mongodb

---
+++ Play.Catalog.Service/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -0,0 +1,21 @@
+﻿using Play.Catalog.Service.Entities;
+using MongoDB.Driver;
+
+namespace Play.Catalog.Service.Repositories;
+
+public class ItemsRepository
+{
+    private const string CollectionName = "items";
+    private readonly IMongoCollection<Item> dbCollection;
+    private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+    public ItemsRepository()
+    {
+        var mongoClient = new MongoClient("mongodb://localhost:2717");
+        var database = mongoClient.GetDatabase("Catalog");
+        dbCollection = database.GetCollection<Item>(CollectionName);
+    }
+
+    public async Task<IReadOnlyCollection<Item>> GetAllAsync() 
+        => await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+}
